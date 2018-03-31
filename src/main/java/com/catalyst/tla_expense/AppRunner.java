082fac8d3e2b88f1a6baffdd0c35c13b5bc6567f
Snerdette <snerdette@gmail.com>
2018-03-31 13:09:39 -0700

    changing repos

---
+++ src/main/java/com/catalyst/tla_expense/AppRunner.java
@@ -0,0 +1,16 @@
+package com.catalyst.tla_expense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main app runner function.
+ */
+@SpringBootApplication
+public class AppRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRunner.class);
+	}
+
+}
